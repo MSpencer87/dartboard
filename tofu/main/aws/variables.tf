4a9a30c356c2b24d5992873317f832cb13f13bee
Michael Spencer <MSpencer87@users.noreply.github.com>
2026-04-07 10:51:58 -0700

    initial commit, aws updates

--- tofu/main/aws/variables.tf
+++ tofu/main/aws/variables.tf
@@ -110,3 +110,15 @@ variable "bastion_host_instance_type" {
   type        = string
   default     = "t4g.large"
 }
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = null
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = null
+}
